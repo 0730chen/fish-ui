347907a8a232f351df182e4b43879052c40450ce
0730chen <1252331215@qq.com>
2020-09-28 23:45:11 +0800

    toRef

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
 <script>
   //ref接受参数并将其包装在具有value属性的对象中，然后将其用于访问或更改反应变量的值
   //在对象内部包装值似乎是不必要的，但是需要使行为在JavaScript中不同数据类型之间保持统一。那是因为在JavaScript中，原始类型（例如Number或）String是通过值而不是通过引用传递的：
-  import { ref,onMounted,watch } from 'vue'
+  import { ref,onMounted,watch,toRefs } from 'vue'
   // 开始使用Composition API，我们首先需要一个可以实际使用它的地方。在Vue组件中，我们将此位置称为setup。
   //setUp使用的值必须要return出去
 
@@ -26,8 +26,10 @@ export default {
 
     const counter = ref(0)
     const repositories = ref([])
+    const reopsitories2 = toRefs([])
 
     console.log(repositories);
+    console.log(reopsitories2);
     console.log(counter) // { value: 0 }
     console.log(counter.value) // 0
 
