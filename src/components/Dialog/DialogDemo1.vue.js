4b8b9f9d641056be33c11f1a6304d8da7a4e7895
0730chen <1252331215@qq.com>
2020-10-11 23:44:03 +0800

    dialog组件显示

--- src/components/Dialog/DialogDemo1.vue.js
+++ src/components/Dialog/DialogDemo1.vue.js
@@ -1,5 +1,23 @@
 import Demo from "../Demo.vue";
+import { Buttons, Dialog } from "../../lib";
 export default {
     name: "DialogDemo1",
-    components: { Demo }
+    components: { Demo, Buttons, Dialog },
+    setup() {
+        const x = ref(false);
+        const toggle = () => {
+            x.value = !x.value;
+        };
+        const f1 = () => {
+            return false;
+        };
+        const f2 = () => {
+        };
+        return {
+            x,
+            toggle,
+            f1,
+            f2
+        };
+    }
 };
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import HelloWorld from './components/HelloWorld.vue';
 import TabsDemo from './components/Tabs/TabsDemo.vue';
 import Demo from "./components/Buttons/Demo.vue";
 import SwitchDemo from "./components/Switch/SwitchDemo.vue";
+import Dialog from "./components/Dialog/DialogDemo1.vue";
 const history = createWebHistory();
 const router = createRouter({
     history: history,
@@ -13,7 +14,8 @@ const router = createRouter({
         { path: '/', component: HelloWorld },
         { path: '/tabs', component: TabsDemo },
         { path: '/demo', component: Demo },
-        { path: '/switch', component: SwitchDemo }
+        { path: '/switch', component: SwitchDemo },
+        { path: '/dialog', component: Dialog }
     ]
 });
 const app = createApp(App);
