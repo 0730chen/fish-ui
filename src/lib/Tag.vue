dacf6af5e1db1790754f46ad28815b4ad577e156
0730chen <1252331215@qq.com>
2020-10-18 23:12:45 +0800

    feat:tag组件æ

--- src/lib/Tag.vue
+++ src/lib/Tag.vue
@@ -1,17 +1,51 @@
 <template>
-    <div>
-        Tag
+    <div class="fish-ui-tag">
+        <slot></slot>
     </div>
 </template>
 
 <script lang="ts">
+    import {
+        computed,
+        ref,
+        watchEffect,
+        onMounted, SetupContext, Component
+    } from 'vue'
+
     export default  {
         name: "Tag",
-        props:{},
-        setup(){}
+        props:{
+            size:{
+                type:String,
+                default:'normal'
+            },
+            color:{
+                type:String,
+                default:'1ABE96',
+            },
+            type:{
+                type:'String',
+                default:'success'
+            }
+        },
+        setup(props){
+            console.log(props);
+            let s = ref(1)
+            console.log(s);
+
+            return {
+                s
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+    $success:#1ABE96;
+    $warn:#BC1A42;
+    $info:#38879B;
+    .fish-ui-tag{
+
+    }
 
 </style>
