13afe4bf97049f191ea89733ce3fd3bdf74f43b1
0730chen <1252331215@qq.com>
2020-10-25 23:18:57 +0800

    feat:新增radio checkout

---
+++ src/lib/Checkbox.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Checkbox
+    </div>
+</template>
+
+<script lang="ts">
+
+    export default {
+        name: "Checkbox"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/lib/Radio.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Radio
+    </div>
+</template>
+
+<script lang="ts">
+
+    @Component
+    export default  {
+        name: "Radio"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
