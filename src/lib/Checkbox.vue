324953832b739d7546fef63b4020431ab6839ec9
0730chen <1252331215@qq.com>
2020-10-26 23:30:26 +0800

    feat:class

--- src/lib/Checkbox.vue
+++ src/lib/Checkbox.vue
@@ -1,13 +1,55 @@
 <template>
-    <div>
+    <div :class="classes">
         Checkbox
     </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup="props">
+    import {computed} from "vue";
 
+    declare const props: {
+        theme?: 'button' | 'text' | 'link';
+        size?: 'normal' | 'big' | 'small';
+        level?: 'normal' | 'main' | 'danger';
+        disabled: boolean;
+        loading: boolean;
+    }
     export default {
-        name: "Checkbox"
+        name: "Checkbox",
+        props: {
+            theme: {
+                type: String,
+                default: "button",
+            },
+            size: {
+                type: String,
+                default: "normal",
+            },
+            level: {
+                type: String,
+                default: "normal",
+            },
+            disabled: {
+                type: Boolean,
+                default: false,
+            },
+            loading: {
+                type: Boolean,
+                default: false
+            }
+        },
+        setup(props){
+
+            const classes = computed(()=>{
+                return {
+                    [`fish-ui-${props.theme}`]:props.theme
+                }
+            })
+
+            return{
+                classes
+            }
+        }
     }
 </script>
 
