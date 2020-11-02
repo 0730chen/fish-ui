7fa20d70fb3ada6a68263d7db4d4198ca71f8723
0730chen <1252331215@qq.com>
2020-11-02 22:42:43 +0800

    icfeat：icon组件布局

--- src/lib/Icon.vue
+++ src/lib/Icon.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
-        Icon
+    <div @click="$emit('click',$event)" :class="classes">
+        <svg class="icon">
+            <use :xlink:href="'#'+IconName"></use>
+        </svg>
     </div>
 </template>
 
+
 <script lang="ts">
-    export default {
-        name:'Icon'
+    import {computed} from 'vue'
+  declare const props: {
+    name:string,
+    class:string
+  }
+
+  export default {
+    name: 'Icon',
+    setup(props){
+      console.log(props)
+      let  classes = computed(()=>{
+        return {
+          'fish-ui-icon':true
+        }
+      })
+      return {classes}
     }
+  }
 </script>
 
 <style lang="scss" scoped>
